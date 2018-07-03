1ea1f4861abddbede85673f0f6af178f0acfb8e6
Ari Brill <aryeh.brill@gmail.com>
2018-07-03 12:16:14 -0400

    Use protocol buffers to store status

--- slow_control_server.py
+++ slow_control_server.py
@@ -8,28 +8,38 @@ input or read-only monitors.
 import configparser
 import socketserver
 
-# Load configuration
+import slow_control_pb2 as sc
+
+# Load server configuration
 config = configparser.ConfigParser()
 config.read('slow_control_config.ini')
-host = config['Networking']['Host']
-port = config['Networking'].getint('Port')
 
-# Start up the server
+# Define status of the server, loading from the configuration file as needed
+server_status = sc.ServerStatus()
+server_status.state = sc.ServerStatus.READY
+server_status.host = config['Networking']['Host']
+server_status.port = config['Networking'].getint('Port')
+
+# Set up the server
 class ServerHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
         # self.rfile and self.wfile are file-like objects created by
         # the handler to read from and write to the client
         while True:
+            # Read the message
             self.message = self.rfile.readline().strip()
+            # If message is empty, close the connection
             if not self.message: return
-            print("{} wrote:".format(self.client_address[0]))
-            print(self.message)
-            self.wfile.write(self.message.upper())
+            # If message is "status", send the status
+            if self.message == b"status":
+                print(server_status)
+            #self.wfile.write(server_state.name.encode())
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 # Start up and run the server
-with ThreadedTCPServer((host, port), ServerHandler) as server:
+with ThreadedTCPServer((server_status.host, server_status.port),
+        ServerHandler) as server:
     server.serve_forever()
