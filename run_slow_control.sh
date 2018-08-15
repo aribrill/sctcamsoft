3d83a4cb9d90ab6f8b9af9f26473157f601b5b33
Ari Brill <aryeh.brill@gmail.com>
2018-08-14 22:56:48 -0400

    Log user output to file

--- run_slow_control.sh
+++ run_slow_control.sh
@@ -2,5 +2,5 @@
 
 CONFIG=$1
 gnome-terminal -- bash -c "python server.py $CONFIG; bash"
-gnome-terminal -- bash -c "python user_output.py $CONFIG; bash"
+gnome-terminal -- bash -c "python -u user_output.py $CONFIG | tee -a slow_control.log; bash"
 gnome-terminal -- bash -c "python user_input.py $CONFIG; bash"
