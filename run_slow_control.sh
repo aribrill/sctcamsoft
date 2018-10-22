0e15a12631ca61ba2c6c420e2510752bd9a7c6cf
Ari Brill <aryeh.brill@gmail.com>
2018-10-22 11:44:53 -0400

    Remove run slow control script
    Superseded by script for running slow control remotely with a local
    option. This script is available separately.

--- run_slow_control.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export NO_AT_BRIDGE=1
-gnome-terminal -- bash -c "source activate sct-slow-control; sudo env 'PATH=$PATH' python server.py config.yml commands.yml; source deactivate; bash" &
-sleep 3
-gnome-terminal -- bash -c "source activate sct-slow-control; python -u user_output.py config.yml; source deactivate; bash" &
-gnome-terminal -- bash -c "source activate sct-slow-control; python user_input.py config.yml commands.yml; source deactivate; bash" &
