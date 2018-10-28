5e0a17b840be74e9eab7432096bc36b611d2b0db
Jake Powell <jakedp7@gmail.com>
2018-10-28 00:04:32 -0500

    Moved server.py run_server to main() method for script calls

--- server.py
+++ server.py
@@ -353,23 +353,28 @@ class ServerController(DeviceController):
                 traceback.print_exc()
                 print('---')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('config_file', help='Path to slow control config file')
-parser.add_argument('commands_file', help='Path to slow control commands file')
-args = parser.parse_args()
 
-with open(args.config_file, 'r') as config_file:
-    config = yaml.load(config_file)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file', help='Path to slow control config file')
+    parser.add_argument('commands_file', help='Path to slow control commands file')
+    args = parser.parse_args()
 
-with open(args.commands_file, 'r') as commands_file:
-    user_commands = yaml.load(commands_file)
+    with open(args.config_file, 'r') as config_file:
+        config = yaml.load(config_file)
 
-devices = {
-#       'server': ServerController --> automatically included as self
-        'fan': FanController,
-        'network': NetworkController,
-        'power': PowerController
-        }
-server = ServerController('server', config, user_commands, devices)
+    with open(args.commands_file, 'r') as commands_file:
+        user_commands = yaml.load(commands_file)
 
-server.run_server()
+    devices = {
+    #       'server': ServerController --> automatically included as self
+            'fan': FanController,
+            'network': NetworkController,
+            'power': PowerController
+            }
+    server = ServerController('server', config, user_commands, devices)
+
+    server.run_server()
+
+if __name__ == "__main__":
+   main()
