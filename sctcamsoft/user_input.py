e163505723d4688be7b42c76be7c7b1ec4d51a9c
Jake Powell <jakedp7@gmail.com>
2018-12-18 22:48:30 -0500

    Implemented client in user input and output scripts

--- sctcamsoft/user_input.py
+++ sctcamsoft/user_input.py
@@ -9,7 +9,7 @@ import shlex
 
 import yaml
 
-import slow_control_pb2 as sc
+from sctcamsoft.slow_control_client import SlowControlClient
 
 # Load configuration
 parser = argparse.ArgumentParser()
@@ -23,38 +23,11 @@ server_host = config['user_interface']['host']
 server_port = config['user_interface']['input_port']
 header_length = config['user_interface']['header_length']
 
-# Open a socket to the slow control server
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((server_host, server_port))
-
 with open(args.commands_file, 'r') as commands_file:
     commands = yaml.load(commands_file)
 
-# Return a serialized message containing the UserCommand corresponding
-# to the input entered by the user. If the command isn't valid, return None.
-def parse_and_serialize_user_input(raw_user_input):
-    raw_user_input = shlex.split(raw_user_input.strip())
-    command = raw_user_input[0]
-    user_input = raw_user_input[1:]
-    if command not in commands:
-        raise ValueError("command '{}' not recognized".format(command))
-    args = commands[command].get('args', {})
-    unspecified_args = [a for a in args if args[a] is None 
-            or isinstance(a, dict) and args[a].get('value') is None]
-    if len(user_input) == len(args):
-        user_args = args
-    elif len(user_input) == len(unspecified_args):
-        user_args = unspecified_args
-    else:
-        raise IndexError("command '{}' requires {} arguments, {} given".format(
-            command, len(args), len(user_input)))
-    message = sc.UserCommand()
-    message.command = command
-    for user_arg, input_value in zip(user_args, user_input):
-        message.args[user_arg] = input_value
-    serialized_message = message.SerializeToString()
-    header = "{{:0{}d}}".format(header_length).format(len(serialized_message))
-    return header.encode() + serialized_message
+sc_client = SlowControlClient(server_host, server_port,
+                              None, header_length, commands)
 
 # Start a terminal for user input
 print("SCT Slow Control - Input")
@@ -69,8 +42,7 @@ while True:
         sock.close()
         break
     try:
-        serialized_command = parse_and_serialize_user_input(raw_user_input)
-        sock.sendall(serialized_command)
+        sc_client.send_command(raw_user_input)
     except (ValueError, IndexError) as e:
         print(e)
         continue
--- sctcamsoft/user_output.py
+++ sctcamsoft/user_output.py
@@ -8,7 +8,7 @@ import socket
 
 import yaml
 
-import slow_control_pb2 as sc
+from sctcamsoft.slow_control_client import SlowControlClient
 
 # Load configuration
 parser = argparse.ArgumentParser()
@@ -21,21 +21,13 @@ server_host = config['user_interface']['host']
 server_port = config['user_interface']['output_port']
 header_length = config['user_interface']['header_length']
 
-# Open a socket to the slow control server
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((server_host, server_port))
+sc_client = SlowControlClient(server_host, None, server_port, 
+                              header_length, None)
 
 # Start a terminal for user input
 print("SCT Slow Control - Output")
 while True:
-    # Parse the incoming message, getting length from the header
-    header = sock.recv(header_length)
-    if header:
-        serialized_message = sock.recv(int(header))
-        if serialized_message:
-            user_update = sc.UserUpdate()
-            user_update.ParseFromString(serialized_message)
-            updates = [(update.device, update.variable, update.value)
-                for update in user_update.updates]
-            for device, variable, value in updates:
-                print("{}: {}: {}".format(device, variable, value))
+    updates = sc_client.recv_updates()
+    if updates is not None:
+        for device, variable, value in updates:
+            print("{}: {}: {}".format(device, variable, value))
