b1fe235e0546ce28cde0542192f0b1f3819e04f3
Jake Powell <jakedp7@gmail.com>
2018-12-18 23:36:11 -0500

    Implemented SlowControlClient in GUI
    Replaced redundant code in ServerIO with calls to shared SlowControlClient

--- sctcamsoft/gui/server_io.py
+++ sctcamsoft/gui/server_io.py
@@ -2,7 +2,7 @@ import socket
 import shlex
 import yaml
 
-from sctcamsoft import slow_control_pb2 as sc
+from sctcamsoft.slow_control_client import SlowControlClient
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 
 class ServerIO(QThread):
@@ -13,77 +13,25 @@ class ServerIO(QThread):
                  server_output_port, header_length, commands):
         QThread.__init__(self)
 
-        self._host = host
-        self._send_cmd_port = server_input_port
-        self._recv_update_port = server_output_port
-        self._header_length = header_length
-
-        self.attempt_connect_send()
-        self.attempt_connect_recv()
-
-        self._commands = commands
-
-    def attempt_connect_send(self):
-        self._send_cmd_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._send_cmd_sock.connect((self._host, self._send_cmd_port))
-
-    def attempt_connect_recv(self):
-        self._recv_update_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._recv_update_sock.connect((self._host, self._recv_update_port))
+        self._sc_client = SlowControlClient(host, server_input_port,
+                                            server_output_port, header_length,
+                                            commands)
 
     def run(self):
         while True:
             try:
-                header = self._recv_update_sock.recv(self._header_length)
-                if header:
-                    serialized_message = self._recv_update_sock.recv(int(header))
-                    if serialized_message:
-                        user_update = sc.UserUpdate()
-                        user_update.ParseFromString(serialized_message)
-                        for update in user_update.updates:
-                            if (update.device == 'ALERT'):
-                                print("ALERT: {}".format(update.variable))
-                            self.on_update.emit(update)
+                updates = self._sc_client.recv_updates()
+                if updates is not None:
+                    for update in updates:
+                        self.on_update.emit(update)
             except (socket.error, ConnectionError):
                 print('Connection to server lost.')
                 self.sleep(5)
-                # print('Attempting to reconnect to rcv socket.')
-                # self.attempt_connect_recv()
 
     @pyqtSlot(str)
     def send_command(self, command):
-        raw_user_input = shlex.split(command.strip())
-        command = raw_user_input[0]
-        user_input = raw_user_input[1:]
-
-        if command not in self._commands:
-            raise ValueError("command '{}' not recognized".format(command))
-        
-        args = self._commands[command].get('args', {})
-        unspecified_args = [a for a in args if args[a] is None 
-                or isinstance(a, dict) and args[a].get('value') is None]
-        if len(user_input) == len(args):
-            user_args = args
-        elif len(user_input) == len(unspecified_args):
-            user_args = unspecified_args
-        else:
-            raise IndexError("command '{}' requires {} arguments, {} given".format(
-                command, len(args), len(user_input)))
-        
-        message = sc.UserCommand()
-        message.command = command
-        for user_arg, input_value in zip(user_args, user_input):
-            message.args[user_arg] = input_value
-            
-        serialized_message = message.SerializeToString()
-        header = "{{:0{}d}}".format(self._header_length).format(len(serialized_message))
-        full_message = header.encode() + serialized_message
-
         try:
-            self._send_cmd_sock.sendall(full_message)
+            self._sc_client.send_command(command)
         except (socket.error, ConnectionError):
             print('Error communicating with server.')
             self.sleep(5)
-            # print('Attempting to reconnect to send_cmd socket.')
-            # print('cmd will not be automatically resent.')
-            # self.attempt_connect_send()
